6e99129844f5a92995b6a0afe685f1bbe230d85f
roycezjq <roycezjq@gmail.com>
2019-02-02 14:49:38 +0800

    FocalLoss.py
    FocalLoss PyTorch

---
+++ FocalLoss.py
@@ -0,0 +1,18 @@
+
+
+class FocalLoss(nn.Module):
+    def __init__(self, gamma=2):
+        super().__init__()
+        self.gamma = gamma
+
+    def forward(self, logit, target):
+        target = target.float()
+        max_val = (-logit).clamp(min=0)
+        loss = logit - logit * target + max_val + \
+               ((-max_val).exp() + (-logit - max_val).exp()).log()
+
+        invprobs = F.logsigmoid(-logit * (target * 2.0 - 1.0))
+        loss = (invprobs * self.gamma).exp() * loss
+        if len(loss.size())==2:
+            loss = loss.sum(dim=1)
+        return loss.mean()
